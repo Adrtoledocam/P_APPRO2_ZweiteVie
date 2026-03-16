88681a2d9f827c86ed9ceb9ae11e302f41276f4c
Adrtoledocam <pq17tmn@gmail.com>
2026-03-16 14:44:45 +0100

    feat: ajout de db.mjs et server.mjs avec connexion MySQL et test API
    - Création du fichier db.mjs pour gérer la connexion MySQL via Docker.
    - Mise en place de server.mjs avec l’initialisation de l’API Express.
    - Ajout d’un testConnection retournant :
      {"message":"Bienvenue sur l'API ZweiteVie !","status":"Running"}
    - Vérification du bon fonctionnement de la connexion, backend et l’API.

--- Backend/app/config/db.mjs
+++ Backend/app/config/db.mjs
@@ -3,57 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
     host : process.env.DB_HOST || 'db',
     port : process.env.DB_PORT || '3306',
-    user : process.env.DB_USER ||'db_root',
-    password : process.env.DB_PASSWORD ||'db_root',
-    database : process.env.DB_NAME || 'prophotostock',
-    waitForConnections : true,
-    connectionLimit : 10,
-})
+    user : process.env.DB_USER || 'db_root',
+    password : process.env.DB_PASSWORD || 'db_root',
+    database : process.env.DB_NAME || 'zweitevie_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-
-/*
-const host= process.env.DB_HOST || 'db';
-//const host= process.env.DB_HOST || 'localhost';
-const port= process.env.DB_PORT || 3306;
-const user= process.env.DB_USER || 'root';
-const password = process.env.DB_PASSWORD || 'root';
-const database = process.env.DB_NAME || "prophotostock";
-
-const dbConfig = {
-    host,
-    port,
-    user,
-    password,
-    database
-};
-
-export const connect = async () => {
-    try
-    {
-        const connection = await mysql.createConnection(dbConfig);
-        console.log('Connected to the database');
-        return connection;
-    }
-    catch (error) {
-        console.error('Error connecting to the database : ', error);
-        throw error;
+//Test
+export const testConnection = async () => {
+    try{
+        const connection = await pool.getConnection();
+        console.log('Connecté à la base de données MySQL');
+        connection.release();
+    } catch (err) {
+        console.error('Erruer de connexion à la base de données :', err.message);
     }
 };
 
-export const databaseConnection = async (req, res, next) => {
-    try 
-    {
-        req.db = await connect();
-        next();
-    } 
-    catch (error) {
-        console.error('Error connection to the database : ', error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-};
-
-*/
-
+export default pool;
--- Backend/app/server.mjs
+++ Backend/app/server.mjs
@@ -1,1 +1,25 @@
-console.log("Hello");
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { testConnection } from './config/db.mjs';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT||8080;
+
+//Middlewares
+app.use(cors());
+app.use(express.json({Limit: '50mb'}));
+app.use(express.urlencoded({extended:true}));
+
+testConnection();
+
+app.get('/', (req, res) => {
+  res.json({ message: "Bienvenue sur l'API ZweiteVie !", status: "Running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+  console.log(`📡 En attente de requêtes de l'application MAUI...`);
+});
